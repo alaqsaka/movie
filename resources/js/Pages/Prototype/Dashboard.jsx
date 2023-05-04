9c8c2ea1d52faaaa057950846eba18bff9201d64
alaqsaka <aqsaabidin15@gmail.com>
2023-05-04 21:38:12 +0700

    featured movie

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import Authenticated from "@/Layouts/Authenticated/Index";
+import Flickity from "react-flickity-component";
+import { Head } from "@inertiajs/react";
+import FeaturedMovie from "@/Components/FeaturedMovie";
 
 const Dashboard = () => {
-    return <Authenticated />;
+    const flickityOptions = {
+        cellAlign: "left",
+        contain: true,
+        groupCells: 1,
+        wrapAround: false,
+        pageDots: false,
+        prevNextButtons: false,
+        draggable: ">1",
+    };
+
+    return (
+        <Authenticated>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://unpkg.com/flickity@2/dist/flickity.min.css"
+                />
+            </Head>
+            <div>
+                <div className="font-semibold text-[22px] text-black mb-4">
+                    Featured Movies
+                </div>
+                <Flickity
+                    className={"carousel"} // default ''
+                    elementType={"div"} // default 'div'
+                    options={flickityOptions} // takes flickity options {}
+                    disableImagesLoaded={false} // default false
+                    reloadOnUpdate // default false
+                    static // default false
+                >
+                    {[1, 2, 3, 4].map((i) => (
+                        <FeaturedMovie
+                            key={i}
+                            slug={"the-batman-in-love"}
+                            name={`The Batman in Love ${i}`}
+                            category={"Comedy"}
+                            thumbnail={"https://picsum.photos/id/1/300/300"}
+                            rating={i + 1}
+                        />
+                    ))}
+                </Flickity>
+            </div>
+        </Authenticated>
+    );
 };
 
 export default Dashboard;
