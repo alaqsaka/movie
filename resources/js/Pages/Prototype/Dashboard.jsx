841e5f442aebab8b4539d574e1dfb8bf8011b1cd
alaqsaka <aqsaabidin15@gmail.com>
2023-05-04 21:49:30 +0700

    slicing dashboard

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -3,6 +3,7 @@ import Authenticated from "@/Layouts/Authenticated/Index";
 import Flickity from "react-flickity-component";
 import { Head } from "@inertiajs/react";
 import FeaturedMovie from "@/Components/FeaturedMovie";
+import MovieCard from "@/Components/MovieCard";
 
 const Dashboard = () => {
     const flickityOptions = {
@@ -18,6 +19,7 @@ const Dashboard = () => {
     return (
         <Authenticated>
             <Head>
+                <title>Dashboard</title>
                 <link
                     rel="stylesheet"
                     href="https://unpkg.com/flickity@2/dist/flickity.min.css"
@@ -46,6 +48,31 @@ const Dashboard = () => {
                         />
                     ))}
                 </Flickity>
+            </div>
+            <div className="mt-[50px]">
+                <div>
+                    <div className="font-semibold text-[22px] text-black mb-4">
+                        Browse
+                    </div>
+                    <Flickity
+                        className={"carousel"} // default ''
+                        elementType={"div"} // default 'div'
+                        options={flickityOptions} // takes flickity options {}
+                        disableImagesLoaded={false} // default false
+                        reloadOnUpdate // default false
+                        static // default false
+                    >
+                        {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
+                            <MovieCard
+                                key={i}
+                                slug={"the-batman-in-love"}
+                                name={`The Batman in Love ${i}`}
+                                category={"Comedy"}
+                                thumbnail={"https://picsum.photos/id/1/300/300"}
+                            />
+                        ))}
+                    </Flickity>
+                </div>
             </div>
         </Authenticated>
     );
