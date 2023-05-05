3572103da16dd90403ed6540550bfb387cd0b185
alaqsaka <aqsaabidin15@gmail.com>
2023-05-05 08:50:42 +0700

    slicing detail movie page

--- resources/js/Pages/Prototype/Movie/Show.jsx
+++ resources/js/Pages/Prototype/Movie/Show.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import ReactPlayer from "react-player";
+import { Link } from "@inertiajs/react";
 
 function Show() {
-    return <div>Show</div>;
+    return (
+        <section
+            class="mx-auto h-screen relative watching-page font-poppins bg-form-bg"
+            id="stream"
+        >
+            <div className="pt-[100px]">
+                <ReactPlayer
+                    url={`https://youtu.be/0DgJ45LQPAs`}
+                    controls
+                    width={"100%"}
+                    height={"85vh"}
+                />
+            </div>
+
+            <div class="absolute top-5 left-5 z-20">
+                <Link href={route("prototype.dashboard")}>
+                    <img
+                        src="/icons/ic_arrow-left.svg"
+                        class="transition-all btn-back w-[46px]"
+                        alt="stream"
+                    />
+                </Link>
+            </div>
+
+            <div class="absolute title-video top-7 left-1/2 -translate-x-1/2 max-w-[310px] md:max-w-[620px] text-center">
+                <span class="font-medium text-2xl transition-all text-white drop-shadow-md select-none">
+                    Details Screen Part Final
+                </span>
+            </div>
+        </section>
+    );
 }
 
 export default Show;
